7ffc0e7023c873de1cdf43f38b769913d9a0e1fd
Lomachynska <123267824+Lomachynska@users.noreply.github.com>
2025-02-02 19:39:01 +0200

    social

--- my-project/src/components/Footer.js
+++ my-project/src/components/Footer.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import './Footer.css'; // Стилі для футера
 
+// Імпортуємо зображення з урахуванням правильного регістру в назвах файлів
+import vectorIcon from '../assets/Vector1.png';
+import facebookIcon from '../assets/facebook.png';
+import instagramIcon from '../assets/instagram.png';
+import twitterIcon from '../assets/twitter.png';
+import youtubeIcon from '../assets/YouTube.png';
+
 function Footer() {
   return (
     <footer className="footer">
-      <div className="logo">Чіп Чендж</div>
-      <div>04128, м.Київ, вул. Хрещатик, 19</div>
-      <div>Ліцензія НБУ №156</div>
-      <div className="services">
+      {/* Перший стовпчик: Логотип та інформація */}
+      <div className="column logo-container">
+        <img src={vectorIcon} alt="Vector Icon" className="vector-icon" />
+        <span className="logo">Чіп Чендж</span>
+        <div className="info">
+          <div>04128, м.Київ, вул. Хрещатик, 19</div>
+          <div>Ліцензія НБУ №156</div>
+        </div>
+      </div>
+
+      {/* Другий стовпчик: Послуги */}
+      <div className="column services">
         <ul>
           <li>Послуги</li>
           <li>Конвертер валют</li>
@@ -15,11 +30,26 @@ function Footer() {
           <li>Задати питання</li>
         </ul>
       </div>
-      <div className="socials">
-        <span>Фейсбук</span>
-        <span>Інстаграм</span>
-        <span>Твіттер</span>
-        <span>Ютуб</span>
+
+      {/* Третій стовпчик: Цілодобова підтримка */}
+      <div className="column support">
+        <div className="phone">3773</div>
+        <div className="support-text">Цілодобова підтримка</div>
+      </div>
+
+      {/* Четвертий стовпчик: Телефон для дзвінків */}
+      <div className="column phone-info">
+        <div>8 800 111 22 33</div>
+        <div>Безкоштовно для дзвінків</div>
+        <div>в межах України</div>
+      </div>
+
+      {/* П'ятий стовпчик: Соціальні мережі (без посилань) */}
+      <div className="column socials">
+        <img src={facebookIcon} alt="Facebook" className="social-icon" />
+        <img src={instagramIcon} alt="Instagram" className="social-icon" />
+        <img src={twitterIcon} alt="Twitter" className="social-icon" />
+        <img src={youtubeIcon} alt="YouTube" className="social-icon" />
       </div>
     </footer>
   );
--- my-project/src/components/MasterCard.js
+++ my-project/src/components/MasterCard.js
@@ -1,13 +1,21 @@
 // src/components/MasterCard.js
 import React from 'react';
 import './MasterCard.css'; // Стилі для конвертера валют
+import groupImage from '../assets/Group78.png'; // Імпортуємо картинку
 
 function MasterCard() {
   return (
     <div className="master">
-      <h2>Конвертер валют</h2>
-      <p>Переважна діяльність банківської <br></br> групи за останні чотири звітні квартали <br></br> становить 50 і більше відсотків.</p>
-      <button className="master-button">Конвертувати валюту</button>
+      <div className="content-container">
+        <div className="text-container">
+          <h2>Конвертер валют</h2>
+          <p>Переважна діяльність банківської <br /> групи за останні чотири звітні квартали <br /> становить 50 і більше відсотків.</p>
+          <button className="master-button">Конвертувати валюту</button>
+        </div>
+        <div className="image-container">
+          <img src={groupImage} alt="Group 78" />
+        </div>
+      </div>
     </div>
   );
 }
