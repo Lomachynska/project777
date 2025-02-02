cec1cf290e3b55a9ecde5990afdc45a8b22793e2
Lomachynska <123267824+Lomachynska@users.noreply.github.com>
2025-02-02 09:09:51 +0200

    5

--- my-project/src/components/CurrencyConverter.js
+++ my-project/src/components/CurrencyConverter.js
@@ -1,13 +1,18 @@
-// src/components/CurrencyConverter.js
 import React from 'react';
 import './CurrencyConverter.css'; // Стилі для конвертера валют
+import standartImage from '../assets/standart2.png'; // Імпортуємо картинку
 
 function CurrencyConverter() {
   return (
     <div className="currency-converter">
-      <h2>Чіп Челендж</h2>
-      <p>Обмінник валют - навчальний</p>
-      <button className="convert-button">Конвертувати</button> {/* Кнопка без функціонала */}
+      <div className="converter-content">
+        <h2>Чіп Челендж</h2>
+        <p>Обмінник валют - навчальний</p>
+        <button className="convert-button">Конвертувати</button> {/* Кнопка без функціонала */}
+      </div>
+      <div className="converter-image">
+        <img src={standartImage} alt="Currency Converter" />
+      </div>
     </div>
   );
 }
--- my-project/src/components/Header.js
+++ my-project/src/components/Header.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './Header.css'; // Підключення стилів
 import { ReactComponent as UserIcon } from '../assets/Vector.svg'; // Імпортуємо SVG як компонент
+import vectorIcon from '../assets/Vector1.png'; // Імпортуємо зображення PNG для іконки
 
 function Header() {
   return (
     <div className="header">
-      <div className="logo">Чіп Челендж</div>
+      <div className="logo">
+        {/* Додаємо зображення перед текстом логотипу */}
+        <img src={vectorIcon} alt="Logo Icon" className="logo-icon" />
+        Чіп Челендж
+      </div>
       <div className="nav">
         <a href="/">Послуги</a>
         <a href="/about">Конвертор валют</a>
